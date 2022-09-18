964ee4e7bee3898cd536bcc105b0fc49b91a11e3
Aditi Chandra <37157534+AditiChandra18@users.noreply.github.com>
2022-09-18 17:07:22 +0530

    Add files via upload

---
+++ Sum and Height ofNodes BT.java
@@ -0,0 +1,86 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            
+            this.data=data;
+            this.left=null;
+            this.right=null;
+            
+        }
+    }
+    
+    static class Binarytree{
+        static int idx=-1;
+         public static Node buildtree(int nodes[]){
+             idx++;
+             
+             if(nodes[idx]==-1) return null;
+             
+             Node newNode= new Node(nodes[idx]);
+             newNode.left= buildtree(nodes);
+             newNode.right= buildtree(nodes);
+             
+             return newNode;
+    }
+    }
+    
+    public static int countofNodes(Node root){
+        
+        if(root==null) return 0; //mention return 0 
+        
+        int leftNodes=countofNodes(root.left);
+        int rightNodes=countofNodes(root.right);
+        
+        return leftNodes+rightNodes+1;
+        
+    }
+    
+    public static int sumofNodes(Node root){
+        
+        if(root==null) return 0; //mention return 0 
+        
+        int sumleftNodes = sumofNodes(root.left);
+        int sumrightNodes = sumofNodes(root.right);
+        
+        return sumleftNodes+sumrightNodes+root.data;
+        
+    }
+    
+    public static int height(Node root) {
+       if(root == null) {
+           return 0;
+       }
+ 
+       int leftHeight = height(root.left);
+       int rightHeight = height(root.right);
+       return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+   
+	public static void main(String[] args) {
+	    int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+	    
+	    Binarytree tree= new Binarytree();
+	    
+	    Node root=tree.buildtree(nodes);
+	    
+	    System.out.print(sumofNodes(root));
+	    System.out.println("height= "+ height(root));
+
+	
+	}
+	
+}
